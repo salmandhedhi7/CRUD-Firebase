771bce3a9388a20c759d98397c5ecfd1251b9730
salmandhedhi7 <m.shahid.dhedhi@gmail.com>
2023-08-19 10:44:35 +0500

    commits

--- server.mjs
+++ server.mjs
@@ -1,6 +1,14 @@
-console.log("Hy");
-console.log("Hy");
-console.log("Hy");
-console.log("Hy");
-console.log("Hy");
-console.log("Hy");
+import Express  from "express";
+
+
+
+
+//     /static/vscode_windows.exe
+app.use("/static", express.static(path.join(__dirname, 'static')))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Example server listening on port ${PORT}`)
+})
